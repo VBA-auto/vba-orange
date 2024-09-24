3597adb44fc9308a805175515548b9b6a79ea7c0
Chowon-Hasan <chowonhasan7@gmail.com>
2024-09-24 22:52:49 +0600

    sdfsdfs

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -3,46 +3,9 @@ import Head from "next/head";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { useEffect, useRef } from "react";
 import Link from "next/link";
 
 const Hero = () => {
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const videoElement = videoRef.current;
-      const rect = videoElement.getBoundingClientRect();
-      const windowHeight = window.innerHeight;
-
-      if (rect.top <= windowHeight && rect.bottom >= 0) {
-        videoElement.classList.add("video-default");
-        videoElement.classList.remove("video-3d");
-      } else {
-        videoElement.classList.add("video-3d");
-        videoElement.classList.remove("video-default");
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  useEffect(() => {
-    const autoScrollToVideo = () => {
-      const videoElement = videoRef.current;
-      if (videoElement) {
-        videoElement.scrollIntoView({ behavior: "smooth" });
-      }
-    };
-
-    const timer = setTimeout(autoScrollToVideo, 1000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   const settings = {
     className: "center",
     centerMode: true,
@@ -61,50 +24,13 @@ const Hero = () => {
         {/* <meta name="description" content={pageDescription} /> */}
       </Head>
 
-      <div className="container mx-auto py-16 text-center">
+      <div className="container mx-auto py-12 text-center">
         <div className="">
           {/* <div className="bg-black absolute  text-center top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 z-50"> */}
-          <div className="">
-            <div className="">
-              <div className="">
-                <h1 className="text-2xl text-gray-700 font-semibold mb-2 ">
-                  VBA Heading Title
-                </h1>
-              </div>
-              <div className="w-[300px] mx-auto my-5">
-                <Slider {...settings}>
-                  <div className="">
-                    <h3 className="heroTitle">Diagnostic </h3>
-                  </div>
 
-                  <div>
-                    <h3 className="heroTitle">Vidange</h3>
-                  </div>
-
-                  <div>
-                    <h3 className="heroTitle">Réparation</h3>
-                  </div>
-                </Slider>
-              </div>
-              <div className="mb-8 w-1/2 mx-auto">
-                <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Dolore
-                </p>
-                <div className="">
-                  <Link href="aide-en-ligne">
-                    <button className="px-5 text-[15px] py-2 border border-[#3775cd]  text-[#3775cd] hover:bg-[#3775cd] hover:text-white hover:border rounded-md hidden md:block mx-auto mt-5">
-                      Aide en ligne
-                    </button>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          </div>
           <div className=" ">
-            <div className="w-3/4 mx-auto">
+            <div className="w-3/4 mx-auto relative">
               <video
-                ref={videoRef}
                 preload="metadata"
                 autoPlay
                 loop
@@ -113,6 +39,32 @@ const Hero = () => {
               >
                 <source src="/images/video.webm" type="video/webm" />
               </video>
+              <div className="absolute top-0 w-full h-full  z-50 bg-black/50">
+                <div className="absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 z-50">
+                  <div className="w-[300px] mx-auto my-5">
+                    <Slider {...settings}>
+                      <div className="">
+                        <h3 className="heroTitle">Diagnostic </h3>
+                      </div>
+
+                      <div>
+                        <h3 className="heroTitle">Vidange</h3>
+                      </div>
+
+                      <div>
+                        <h3 className="heroTitle">Réparation</h3>
+                      </div>
+                    </Slider>
+                  </div>
+                  <div className="">
+                    <Link href="aide-en-ligne">
+                      <button className="px-5 text-[15px] py-2 border border-white bg-white  text-[#3775cd] hover:bg-[#3775cd] hover:text-white hover:border rounded-md hidden md:block mx-auto mt-5">
+                        Aide en ligne
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
