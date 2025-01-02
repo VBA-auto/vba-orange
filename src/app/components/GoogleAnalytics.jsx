b049af06817d4b7d25db1e80f69c5acf567a8f4d
Chowon-Hasan <vidangeboiteauto@gmail.com>
2025-01-02 15:19:11 +0600

    asdasd

--- src/app/components/GoogleAnalytics.jsx
+++ src/app/components/GoogleAnalytics.jsx
@@ -1,29 +1,69 @@
+// "use client";
+// import { useEffect } from "react";
+
+// const GoogleAnalytics = () => {
+//   useEffect(() => {
+
+//     const headScript = document.createElement("script");
+//     headScript.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+//       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+//       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+//       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+//     })(window,document,'script','dataLayer','GTM-PVCWMCFC');`;
+
+//     const bodyIframe = document.createElement("noscript");
+//     bodyIframe.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PVCWMCFC"
+//       height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
+
+//     document.head.appendChild(headScript);
+//     document.body.appendChild(bodyIframe);
+
+//     return () => {
+
+//       document.head.removeChild(headScript);
+//       document.body.removeChild(bodyIframe);
+//     };
+//   }, []);
+
+//   return null;
+// };
+
+// export default GoogleAnalytics;
 "use client";
 import { useEffect } from "react";
 
 const GoogleAnalytics = () => {
   useEffect(() => {
-    // Google Tag Manager (section HEAD)
+    // Add GTM Script to Head
     const headScript = document.createElement("script");
-    headScript.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    headScript.type = "text/javascript";
+    headScript.async = true;
+    headScript.innerHTML = `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': 
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-PVCWMCFC');`;
+      })(window,document,'script','dataLayer','GTM-PVCWMCFC');
+    `;
 
-    // Google Tag Manager (section BODY)
+    // Add GTM Iframe to Body
     const bodyIframe = document.createElement("noscript");
-    bodyIframe.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PVCWMCFC"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
+    bodyIframe.innerHTML = `
+      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PVCWMCFC"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    `;
 
-    // Append scripts to the head and body
+    // Append Scripts
     document.head.appendChild(headScript);
-    document.body.appendChild(bodyIframe);
+    document.body.insertAdjacentHTML("afterbegin", bodyIframe.outerHTML);
 
     return () => {
-      // Clean up on unmount
+      // Clean up Scripts
       document.head.removeChild(headScript);
-      document.body.removeChild(bodyIframe);
+      const iframe = document.body.querySelector(
+        'iframe[src*="googletagmanager.com/ns.html"]'
+      );
+      if (iframe) iframe.remove();
     };
   }, []);
 
