231319c22fd2f7959226737381ea7784ea53d012
Chowon-Hasan <vidangeboiteauto@gmail.com>
2025-01-02 15:23:42 +0600

    dasdasd

--- src/app/components/GoogleAnalytics.jsx
+++ src/app/components/GoogleAnalytics.jsx
@@ -29,45 +29,33 @@
 // };
 
 // export default GoogleAnalytics;
-"use client";
-import { useEffect } from "react";
+import Script from "next/script";
 
-const GoogleAnalytics = () => {
-  useEffect(() => {
-    // Add GTM Script to Head
-    const headScript = document.createElement("script");
-    headScript.type = "text/javascript";
-    headScript.async = true;
-    headScript.innerHTML = `
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': 
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-PVCWMCFC');
-    `;
+const GoogleAnalytics = () => (
+  <>
+    {/* Google Tag Manager (HEAD section) */}
+    <Script
+      id="gtm-head"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': 
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PVCWMCFC');`,
+      }}
+    />
 
-    // Add GTM Iframe to Body
-    const bodyIframe = document.createElement("noscript");
-    bodyIframe.innerHTML = `
-      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PVCWMCFC"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe>
-    `;
-
-    // Append Scripts
-    document.head.appendChild(headScript);
-    document.body.insertAdjacentHTML("afterbegin", bodyIframe.outerHTML);
-
-    return () => {
-      // Clean up Scripts
-      document.head.removeChild(headScript);
-      const iframe = document.body.querySelector(
-        'iframe[src*="googletagmanager.com/ns.html"]'
-      );
-      if (iframe) iframe.remove();
-    };
-  }, []);
-
-  return null;
-};
+    {/* Google Tag Manager (BODY section) */}
+    <noscript>
+      <iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-PVCWMCFC"
+        height="0"
+        width="0"
+        style={{ display: "none", visibility: "hidden" }}
+      />
+    </noscript>
+  </>
+);
 
 export default GoogleAnalytics;
--- src/app/layout.js
+++ src/app/layout.js
@@ -5,10 +5,7 @@ import SubHeader from "./components/SubHeader";
 import Footer from "./components/Footer";
 import CookieConsent from "./components/CookieConsent";
 import GoogleAnalytics from "./components/GoogleAnalytics";
-
-{
-  <link rel="icon" href="/favicon.ico" sizes="any" />;
-}
+import Script from "next/script";
 
 export const metadata = {
   title: "VBA - Laboiteauto",
@@ -21,12 +18,50 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
+      <head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+
+        {/* Google Tag Manager (HEAD section) */}
+        <Script
+          id="gtm-head"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': 
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PVCWMCFC');`,
+          }}
+        />
+      </head>
       <body>
+        {/* Google Tag Manager (BODY section) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PVCWMCFC"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
+        {/* SubHeader */}
         <SubHeader />
+
+        {/* MainHeader */}
         <MainHeader />
+
+        {/* Page Content */}
         {children}
+
+        {/* Google Analytics */}
         <GoogleAnalytics />
+
+        {/* Cookie Consent */}
         <CookieConsent />
+
+        {/* Footer */}
         <Footer />
       </body>
     </html>
