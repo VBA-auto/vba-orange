0b61ad8ffc645030ec34403a5371d649748c93f0
Chowon-Hasan <vidangeboiteauto@gmail.com>
2025-05-03 14:20:31 +0600

    unoptimised

--- src/app/components/Navbar/MainHeader.jsx
+++ src/app/components/Navbar/MainHeader.jsx
@@ -123,8 +123,8 @@ const MainHeader = () => {
               <Image
                 width={80}
                 height={80}
-                src="https://res.cloudinary.com/ddf0yuahe/image/upload/v1746259961/VBAlogo_frschi.png"
-                // src="/images/VBAlogo.webp"
+                src="/images/VBAlogo.webp"
+                unoptimized
                 alt="logo"
               />
             </Link>
